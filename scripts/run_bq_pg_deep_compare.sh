7f151e08e3ff976acacf28a22f0d683a94f4671a
Thomas Kaliakos <tkaliakos@gmail.com>
2023-07-15 22:45:48 +0300

    Add bash scripts

---
+++ scripts/run_bq_pg_deep_compare.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+DATE=$(date +"%s")
+CHECK=$((DATE / 86400 % 5 ))
+if [ $CHECK -ne 3 ]; then
+    echo "Not an epoch boundary. Exiting..."
+    exit 0
+else
+    echo "Epoch boundary, running BQ/PG deep comparison"
+fi
+
+export PGPASSWORD=$(jq -r .password <<< "$DB_CONFIG")
+export PGHOST=$(jq -r .host <<< "$DB_CONFIG")
+export PGDATABASE=$(jq -r .dbname <<< "$DB_CONFIG")
+export PGDATABASE_TESTNET=$(jq -r .dbname_test <<< "$DB_CONFIG")
+export PGPORT=$(jq -r .port <<< "$DB_CONFIG")
+export PGUSER=$(jq -r .username <<< "$DB_CONFIG")
+
+export BQUSER=$(jq -r .client_email <<< "$BQ_CONFIG")
+export BQPROJECT=$(jq -r .project_id <<< "$BQ_CONFIG")
+echo $BQ_CONFIG > /usr/src/app/scripts/key.json
+
+EPOCH_NO=$1
+python3 ./deep_compare/bq_pg_deep_compare.py $EPOCH_NO
+#aws sns publish --topic-arn $SNS_TOPIC_ARN --message file://msg.txt

1a5aacd3b77ba95701306b8344832fdff3149f5f
Blockchain Data Analytics <bca@sbclab.net>
2024-10-22 17:12:05 +0300

    cleanups

--- scripts/run_bq_update.sh
+++ scripts/run_bq_update.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-# this script expects environment variables $BQ_CONFIG and $DB_CONFIG to be set beforehand
+# this script expects environment variable $BQ_CONFIG to be set beforehand
 
 set -e
+
+[ -n "${BQ_CONFIG}" ] || { echo "missing \$BQ_CONFIG"; exit 1; }
 
 BASEDIR=$(realpath $(dirname $0))
 TEMPDIR=$(mktemp -d)
 
 source ${BASEDIR}/conf/config.pg
 source ${BASEDIR}/conf/config.bq
-echo $BASEDIR
-echo $TEMPDIR
-echo $(which bash)
 
 export BQUSER=$(jq -r .client_email <<< "$BQ_CONFIG")
 echo $BQ_CONFIG > ${TEMPDIR}/key.json
@@ -22,26 +21,24 @@ ENDING_SLOT=$(echo ${res2} | ${SED} -ne 's/^max_slot | max_epoch --*+--* \([0-9]
 PG_EPOCH=$(echo ${res2} | ${SED} -ne 's/^max_slot | max_epoch --*+--* \([0-9][0-9]*\) | \([0-9][0-9]*\).*/\2/p;')
 ENDING_SLOT_MINUS_GRACE=$((ENDING_SLOT - GRACE_SLOTS))
 
-declare -a Tables=("tx" "tx_in_out" "tx_consumed_output" "tx_hash" "tx_metadata" "block" "block_hash" "rel_addr_txout" "rel_stake_txout" "rel_stake_hash" "collateral" "ma_minting" "script" "pool_offline_data" "pool_owner" "pool_retire" "pool_update" "redeemer" "stake_registration" "stake_deregistration" "withdrawal" "delegation" "datum" )
-# use for loop to read all tables
-for (( i=0; i<${#Tables[@]}; i++ ));
-do
-  TABLE=${Tables[$i]}
-  TABLNAME="${BQ_PROJECT}.cardano_mainnet.${TABLE}"
-  res=$(${BQ} --format=json query --nouse_legacy_sql "SELECT last_slot_no FROM ${BQ_PROJECT}.db_sync.last_index where tablename = '${TABLNAME}'")
+for TABLE in tx tx_in_out tx_consumed_output tx_hash tx_metadata block block_hash rel_addr_txout rel_stake_txout rel_stake_hash collateral ma_minting script pool_offline_data pool_owner pool_retire pool_update redeemer stake_registration stake_deregistration withdrawal delegation datum; do
+  TABLENAME="${BQ_PROJECT}.cardano_mainnet.${TABLE}"
+  echo $TABLENAME
+  res=$(${BQ} --format=json query --nouse_legacy_sql "SELECT last_slot_no FROM ${BQ_PROJECT}.db_sync.last_index where tablename = '${TABLENAME}'")
   STARTING_SLOT=$(echo ${res} | jq -r '.[0].last_slot_no')
   COUNT=0
   while [ "$COUNT" -lt 3 ] && [ "$STARTING_SLOT" -lt "$ENDING_SLOT_MINUS_GRACE" ]
   do
     STARTING_SLOT=$(echo ${res} | jq -r '.[0].last_slot_no')
     SCRIPT="./update_${TABLE}.sh"
-    echo "Updating ${TABLNAME} since ${STARTING_SLOT} slot until ${ENDING_SLOT}"
+    echo "Updating ${TABLENAME} since ${STARTING_SLOT} slot until ${ENDING_SLOT}"
     ${SCRIPT} ${STARTING_SLOT} ${ENDING_SLOT}
-    res=$(${BQ} --format=json query --nouse_legacy_sql "SELECT last_slot_no FROM ${BQ_PROJECT}.db_sync.last_index where tablename = '${TABLNAME}'")
+    res=$(${BQ} --format=json query --nouse_legacy_sql "SELECT last_slot_no FROM ${BQ_PROJECT}.db_sync.last_index where tablename = '${TABLENAME}'")
     STARTING_SLOT=$(echo ${res} | jq -r '.[0].last_slot_no')
     COUNT=$((COUNT + 1))
   done
 done
+
 echo "Updating db-sync slot_no to ${ENDING_SLOT} and epoch_no to ${PG_EPOCH} in BigQuery"
 Q="UPDATE ${BQ_PROJECT}.db_sync.last_index set last_slot_no=${ENDING_SLOT}, last_epoch_no=${PG_EPOCH} WHERE tablename='db-sync';"
 ${BQ} query --nouse_legacy_sql "${Q}"
--- scripts/run_update_epoch.sh
+++ scripts/run_update_epoch.sh
@@ -30,23 +30,19 @@ echo $BQ_CONFIG > ${TEMPDIR}/key.json
 gcloud auth activate-service-account $BQUSER --key-file ${TEMPDIR}/key.json 
 ${BQ} ls
 
-declare -a Tables=("epoch_param" "ada_pots" "param_proposal" "ma_minting" "pool_update" "pool_offline_data" "delegation" "reward" "rel_addr_txout" "rel_stake_txout" "epoch_stake" )
- 
 # use for loop to read all tables
-for (( i=0; i<${#Tables[@]}; i++ ));
+for TABLE in epoch_param ada_pots param_proposal ma_minting pool_update pool_offline_data delegation reward rel_addr_txout rel_stake_txout epoch_stake;
 do
-  TABLE=${Tables[$i]}
-  TABLNAME="${BQ_PROJECT}.cardano_mainnet.${TABLE}"
-  res=$(${BQ} --format=json query --nouse_legacy_sql "SELECT last_epoch_no FROM ${BQ_PROJECT}.db_sync.last_index where tablename = '${TABLNAME}'")
+  TABLENAME="${BQ_PROJECT}.cardano_mainnet.${TABLE}"
+  res=$(${BQ} --format=json query --nouse_legacy_sql "SELECT last_epoch_no FROM ${BQ_PROJECT}.db_sync.last_index where tablename = '${TABLENAME}'")
   BQ_EPOCH_NO=$(echo ${res} | jq -r '.[0].last_epoch_no')
   
   res2=$(${PSQL} -c "SELECT max(epoch_no) as max_epoch_no from public.block;")
   PG_EPOCH_NO=$(echo ${res2} | ${SED} -ne 's/^max_epoch_no --* \([0-9][0-9]*\).*/\1/p;')
   
-  if [ "${PG_EPOCH_NO}" -gt "${BQ_EPOCH_NO}" ]
-  then
+  if [ "${PG_EPOCH_NO}" -gt "${BQ_EPOCH_NO}" ]; then
     SCRIPT="./update_epoch_${TABLE}.sh"
-    echo "Updating ${TABLNAME}. BigQuery epoch: ${BQ_EPOCH_NO} - Postgres epoch: ${PG_EPOCH_NO}"
+    echo "Updating ${TABLENAME}. BigQuery epoch: ${BQ_EPOCH_NO} - Postgres epoch: ${PG_EPOCH_NO}"
     ${SCRIPT} ${BQ_EPOCH_NO} ${PG_EPOCH_NO}
   fi
 done

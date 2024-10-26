6fc0ee52f834bdf14547a49b61244347cbec2060
Alexander Diemand <codieplusplus@apax.net>
2024-10-26 18:50:41 +0000

    copies of old vw_bq_param_proposal and vw_bq_epoch_param (#20)
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- schema/views/vw_bq_param_proposal.sql
+++ schema/views/vw_bq_param_proposal.sql
@@ -3,40 +3,45 @@
 -- DROP VIEW analytics.vw_bq_param_proposal;
 
 CREATE OR REPLACE VIEW analytics.vw_bq_param_proposal
-AS
-SELECT pp.epoch_no,
-       encode(key,'hex') AS key,
-       min_fee_a, min_fee_b,
-       max_block_size,
-       max_tx_size,
-       max_bh_size,
-       key_deposit, pool_deposit,
-       max_epoch,
-       optimal_pool_count,
-       influence,
-       monetary_expand_rate,
-       treasury_growth_rate,
-       decentralisation,
-       encode(entropy,'hex') AS entropy,
-       protocol_major, protocol_minor,
-       min_utxo_value,
-       min_pool_cost,
-       coins_per_utxo_size,
-       cm.costs AS cost_model,
-       price_mem,
-       price_step,
-       max_tx_ex_mem, max_tx_ex_steps,
-       max_block_ex_mem, max_block_ex_steps,
-       max_val_size,
-       collateral_percent,
-       max_collateral_inputs,
-       block.slot_no AS registered_tx_slot_no,
-       tx.block_index AS registered_tx_index
-FROM public.param_proposal pp
-         JOIN public.tx tx ON tx.id = registered_tx_id
-         JOIN public.block block ON block.id = tx.block_id
-         LEFT JOIN public.cost_model cm ON cm.id = cost_model_id
-ORDER BY pp.epoch_no, block.slot_no, tx.block_index, pp.key ASC;
+ AS
+ SELECT pp.epoch_no,
+    encode(pp.key::bytea, 'hex'::text) AS key,
+    pp.min_fee_a,
+    pp.min_fee_b,
+    pp.max_block_size,
+    pp.max_tx_size,
+    pp.max_bh_size,
+    pp.key_deposit,
+    pp.pool_deposit,
+    pp.max_epoch,
+    pp.optimal_pool_count,
+    pp.influence,
+    pp.monetary_expand_rate,
+    pp.treasury_growth_rate,
+    pp.decentralisation,
+    encode(pp.entropy::bytea, 'hex'::text) AS entropy,
+    pp.protocol_major,
+    pp.protocol_minor,
+    pp.min_utxo_value,
+    pp.min_pool_cost,
+    pp.coins_per_utxo_size,
+    cm.costs AS cost_model,
+    pp.price_mem,
+    pp.price_step,
+    pp.max_tx_ex_mem,
+    pp.max_tx_ex_steps,
+    pp.max_block_ex_mem,
+    pp.max_block_ex_steps,
+    pp.max_val_size,
+    pp.collateral_percent,
+    pp.max_collateral_inputs,
+    block.slot_no AS registered_tx_slot_no,
+    tx.block_index AS registered_tx_index
+   FROM param_proposal pp
+     JOIN tx tx ON tx.id = pp.registered_tx_id
+     JOIN block block ON block.id = tx.block_id
+     LEFT JOIN cost_model cm ON cm.id = pp.cost_model_id
+  ORDER BY pp.epoch_no, block.slot_no, tx.block_index, pp.key;
 
 ALTER TABLE analytics.vw_bq_param_proposal
     OWNER TO cardano;

8383bb97980644c978a21d2b1be14fc3754b4c3c
Alexander Diemand <codieplusplus@apax.net>
2024-10-26 20:35:10 +0200

    copies of old vw_bq_param_proposal and vw_bq_epoch_param
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- schema/views/vw_bq_epoch_param.sql
+++ schema/views/vw_bq_epoch_param.sql
@@ -3,26 +3,41 @@
 -- DROP VIEW analytics.vw_bq_epoch_param;
 
 CREATE OR REPLACE VIEW analytics.vw_bq_epoch_param
-AS
-SELECT epoch_no, min_fee_a, min_fee_b,
-       max_block_size, max_tx_size, max_bh_size,
-       key_deposit, pool_deposit, max_epoch,
-       optimal_pool_count, influence,
-       monetary_expand_rate, treasury_growth_rate,
-       decentralisation,
-       encode(extra_entropy,'hex') AS extra_entropy,
-       protocol_major, protocol_minor,
-       min_utxo_value, min_pool_cost,
-       encode(nonce,'hex') AS nonce,
-       coins_per_utxo_size, cm.costs as cost_model,
-       price_mem, price_step,
-       max_tx_ex_mem, max_tx_ex_steps,
-       max_block_ex_mem, max_block_ex_steps,
-       max_val_size,
-       collateral_percent, max_collateral_inputs
-FROM public.epoch_param ep
-LEFT JOIN public.cost_model cm ON cm.id = cost_model_id
-ORDER BY epoch_no ASC;
+ AS
+ SELECT ep.epoch_no,
+    ep.min_fee_a,
+    ep.min_fee_b,
+    ep.max_block_size,
+    ep.max_tx_size,
+    ep.max_bh_size,
+    ep.key_deposit,
+    ep.pool_deposit,
+    ep.max_epoch,
+    ep.optimal_pool_count,
+    ep.influence,
+    ep.monetary_expand_rate,
+    ep.treasury_growth_rate,
+    ep.decentralisation,
+    encode(ep.extra_entropy::bytea, 'hex'::text) AS extra_entropy,
+    ep.protocol_major,
+    ep.protocol_minor,
+    ep.min_utxo_value,
+    ep.min_pool_cost,
+    encode(ep.nonce::bytea, 'hex'::text) AS nonce,
+    ep.coins_per_utxo_size,
+    cm.costs AS cost_model,
+    ep.price_mem,
+    ep.price_step,
+    ep.max_tx_ex_mem,
+    ep.max_tx_ex_steps,
+    ep.max_block_ex_mem,
+    ep.max_block_ex_steps,
+    ep.max_val_size,
+    ep.collateral_percent,
+    ep.max_collateral_inputs
+   FROM epoch_param ep
+     LEFT JOIN cost_model cm ON cm.id = ep.cost_model_id
+  ORDER BY ep.epoch_no;
 
 ALTER TABLE analytics.vw_bq_epoch_param
     OWNER TO cardano;

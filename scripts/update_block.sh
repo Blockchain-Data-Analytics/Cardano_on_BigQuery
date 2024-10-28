85a0f5438cb04191b34f17a07745964eae539368
Blockchain Analytics <monoid-is-a-monad@proton.me>
2024-10-28 19:29:41 +0200

    Add block hash into block

--- scripts/update_block.sh
+++ scripts/update_block.sh
@@ -45,7 +45,7 @@ DATASETID="${BQ_PROJECT}:db_sync"
 ## 2 insert slots (clean until max) into table: tmp_block_1
 TMPTBL="tmp_block_1"
 Q="
-  SELECT epoch_no, slot_no, block_time, block_size, tx_count, sum_tx_fee, script_count, sum_script_size, pool_hash
+  SELECT epoch_no, slot_no, block_time, block_size, tx_count, sum_tx_fee, script_count, sum_script_size, pool_hash, block_hash
   FROM analytics.vw_bq_block
   WHERE slot_no >= ${CLEAN_SLOT_NO}
    AND slot_no <= ${MAX_SLOT_NO}"
